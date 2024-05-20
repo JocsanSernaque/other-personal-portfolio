1574b91a711c56881e2cafa39aa9583b1d745c25
jocsanDev_local <jocsansernaque@gmail.com>
2024-05-20 12:20:14 -0500

    Se creo la sección de inicio

---
+++ tailwind.config.jsx
@@ -0,0 +1,17 @@
+tailwind.config = {
+      theme: {
+        extend: {
+          boxShadow: {
+                '2xl': '5px 5px 30px -10px rgba(239, 231, 231, 0.6)'
+          },
+          fontFamily: {
+            orbitron: ["Orbitron", "sans-serif"],
+            bowlby: ["Bowlby One", "sans-serif"]
+          },
+          colors: {
+            bg: '#1C1B1B',
+            text: '#F4EFEF',
+          }
+        }
+      }
+}
